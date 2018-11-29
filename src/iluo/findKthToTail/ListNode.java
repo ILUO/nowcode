d7da0f505226e8baff22aacddfc6e2de56aad4cc
ILUO <2323221725@qq.com>
2018-11-29 09:30:03 +0800

    findKthToTail

---
+++ src/iluo/findKthToTail/ListNode.java
@@ -0,0 +1,10 @@
+package iluo.findKthToTail;
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/iluo/findKthToTail/Solution.java
@@ -0,0 +1,54 @@
+package iluo.findKthToTail;
+
+
+
+public class Solution {
+
+    public ListNode FindKthToTail(ListNode head, int k) {
+
+        if(k == 0 || head == null){
+            return null;
+        }
+
+        ListNode pNode = head;
+        ListNode qNode = head;
+
+
+        for(int count = 0;count < k-1;){
+            qNode = qNode.next;
+            if(qNode == null){
+                return null;
+            }
+            count++;
+        }
+
+        for(;qNode.next!=null;){
+            pNode = pNode.next;
+            qNode = qNode.next;
+        }
+
+        return pNode;
+    }
+
+    public static void main(String []args){
+        Solution solution = new Solution();
+        ListNode head = new ListNode(0);
+        ListNode node;
+        node = head;
+        for(int i = 1;i <= 0;i++){
+            ListNode temp = new ListNode(i);
+            temp.next = null;
+            node.next = temp;
+            node = temp;
+        }
+        int k = 1;
+
+        ListNode listNode = solution.FindKthToTail(null,k);
+        if(listNode == null){
+            System.out.println(listNode);
+        }else {
+            System.out.println(solution.FindKthToTail(head,k).val);
+        }
+
+    }
+}
